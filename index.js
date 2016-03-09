a626153ce53cf7c8ed4a718c50c52c55047ff053
Mark Steve Samson <hello@marksteve.com>
2016-03-09 14:22:06 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Flex, Box} from 'reflexbox'
+import {
+  Panel,
+  PanelHeader,
+  Text
+} from 'rebass'
+
+const Project = ({name}) => {
+  return (
+    <Box col={3} p={2}>
+      <Panel>
+        <PanelHeader>{name}</PanelHeader>
+      </Panel>
+    </Box>
+  )
+}
+
+class App extends React.Component {
+  render() {
+    let {projects} = this.props
+    return (
+      <Box p={2}>
+        <Box px={2}>
+          <h1>New tab</h1>
+        </Box>
+        <Flex align='flex-start'>
+          {projects.map(Project)}
+        </Flex>
+      </Box>
+    )
+  }
+}
+
+var testProps = {
+  projects: [
+    {name: "Personal"},
+    {name: "Blank"},
+    {name: "PythonPH"},
+    {name: "Incorgito"},
+  ],
+}
+
+ReactDOM.render(<App {...testProps} />, document.getElementById('app'))
